5cc16d68645be867bce1bdffd502d2f43c670a95
오승우 <dhtmddn13213@cnspartner.com>
2022-05-25 13:52:46 +0900

    Login & get Home foler Contents

--- FourTierSharp/FourTierSharp/MainWindow.xaml.cs
+++ FourTierSharp/FourTierSharp/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using FourTierSharp.TcSession;
+using Teamcenter.Services.Strong.Core;
+using Teamcenter.Soa.Client.Model;
 using Teamcenter.Soa.Client.Model.Strong;
 using Session = FourTierSharp.TcSession.Session;
 
@@ -24,10 +26,34 @@ namespace FourTierSharp
     /// </summary>
     public partial class MainWindow : Window
     {
-        [Obsolete]
+        public TCClient Client { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [Obsolete]
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Client = new TCClient(TB_ID.Text, TB_PW.Text);
+            MessageBox.Show(Client.CurrentUser.User_name);
+            DataManagementService dmService = DataManagementService.getService(Session.GetConnection());
+
+            Folder homeFolder = Client.CurrentUser.Home_folder;
+            ModelObject[] objects = { homeFolder };
+            string[] attributes = { "contents" };
+            dmService.GetProperties(objects, attributes);
+            var contents = homeFolder.Contents;
+            string msg = "";
+
+            string[] attrs = { "object_name", "object_desc", "fnd0objectId" };
+            dmService.GetProperties(contents, attrs);
+
+            foreach (WorkspaceObject o in contents)
+            {
+                msg += String.Format("{0} : {1}\n",o.Uid,o.Object_name);
+            }
+            MessageBox.Show(msg);
         }
     }
 }
